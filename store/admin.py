e721d2cc9ed8a55b15433e602612c8d6bcb9866b
FlorianAuc <florian.aucouturier@gmail.com>
2022-09-02 16:36:57 +0200

    first commit

---
+++ store/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from store.models import Order, Product, Cart
+
+# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Cart)
